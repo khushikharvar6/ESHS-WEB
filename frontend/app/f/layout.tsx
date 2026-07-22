8b4b8f1b707c155b4cea51b9d2d1af090ff3682a
Khushi Kharvar <khushi@example.com>
2026-07-22 13:07:29 +0530

    fix: move google translate widget from global layout to feedback layout only

--- frontend/app/f/layout.tsx
+++ frontend/app/f/layout.tsx
@@ -3,5 +3,16 @@ export default function PublicFeedbackLayout({
 }: {
   children: React.ReactNode
 }) {
-  return <>{children}</>
+  return (
+    <>
+      <div id="google_translate_element" className="absolute top-2 right-2 z-50 print-hide-dialog" />
+      <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" />
+      <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
+        function googleTranslateElementInit() {
+          new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'hi,gu,mr,en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+        }
+      ` }} />
+      {children}
+    </>
+  )
 }
